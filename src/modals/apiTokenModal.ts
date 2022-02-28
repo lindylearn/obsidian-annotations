c795f0cb6e6be3785a8aabcb8c64ee93db4cc68f
Peter Hagen <pha@hey.com>
2022-02-28 15:08:15 +0100

    Fetch user groups on login

--- src/modals/apiTokenModal.ts
+++ src/modals/apiTokenModal.ts
@@ -1,17 +1,21 @@
 import { App, Modal } from 'obsidian';
 import type { TokenManager } from '~/store/tokenManager';
 import ApiTokenModalContent from './apiTokenModal.svelte';
+import SyncGroup from '~/sync/syncGroup';
 
 export default class ApiTokenModal extends Modal {
     public waitForClose: Promise<void>;
     private resolvePromise: () => void;
     private modalContent: ApiTokenModalContent;
     private tokenManager: TokenManager;
+    private syncGroup: SyncGroup;
 
     constructor(app: App, tokenManager: TokenManager) {
         super(app);
 
         this.tokenManager = tokenManager;
+        this.syncGroup = new SyncGroup();
+
         this.waitForClose = new Promise(
             (resolve) => (this.resolvePromise = resolve)
         );
@@ -22,8 +26,10 @@ export default class ApiTokenModal extends Modal {
             target: this.contentEl,
             props: {
                 onSubmit: async (value: string) => {
-                    await this.tokenManager.setToken(value);
+                    await this.tokenManager.setToken(value); // validates token
                     this.close();
+
+                    await this.syncGroup.sync();
                 },
             },
         });
